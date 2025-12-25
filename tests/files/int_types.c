a728ce2ba62fac0fce88e644f0a1c1bcab3b4ad0
Takashiidobe <idobetakashi@gmail.com>
2025-12-25 12:27:44 -0500

    Add integration test for integer types

---
+++ tests/files/int_types.c
@@ -0,0 +1,11 @@
+int main() {
+    char c = -1;
+    unsigned char uc = 200;
+    short s = -123;
+    unsigned short us = 60000;
+    int i = -123456;
+    unsigned int ui = 100000;
+    long l = -300000;
+    unsigned long ul = 300000;
+    return c + uc + s + us + i + ui + l + ul;
+}
