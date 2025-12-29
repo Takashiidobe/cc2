24fd568609b5e2b95ade23a17616dea47e27be05
Takashiidobe <idobetakashi@gmail.com>
2025-12-29 10:27:55 -0500

    clean up asm files

--- tests/files/offsetof_first.s
+++
@@ -1,9 +0,0 @@
-    .globl main
-main:
-    pushq %rbp
-    movq %rsp, %rbp
-    movq $0, %rax
-    jmp .L0
-.L0:
-    popq %rbp
-    ret
--- tests/files/offsetof_struct.s
+++
@@ -1,9 +0,0 @@
-    .globl main
-main:
-    pushq %rbp
-    movq %rsp, %rbp
-    movq $4, %rax
-    jmp .L0
-.L0:
-    popq %rbp
-    ret
